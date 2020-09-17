0839928c745ba95cb4cb7a2e10addf836a7be3f2
zhangzhenfei <fei@sadais.com>
2020-09-17 12:00:02 +0800

    feat: 完善列表项

--- src/pages/base-style/pages/list/index.vue
+++ src/pages/base-style/pages/list/index.vue
@@ -2,19 +2,20 @@
   <view class="pi-scroll-container">
     <pi-navbar title="list" />
     <scroll-view class="pi-scroll" scroll-y>
-      <pi-list>
-        <pi-list-item>a</pi-list-item>
+      <pi-list title="列表面板">
         <pi-list-item title="标题标题" />
         <pi-list-item title="标题标题" desc="我很帅" />
         <pi-list-item title="舔狗" desc="我很可爱" extra-text="舔狗子可爱" />
-        <pi-list-item title="语言切换（中文）">
-          <pi-checkbox slot="right" active-mode="fill" shape="round" :value="true" />
-        </pi-list-item>
       </pi-list>
 
-      <pi-list-item title="单独使用item , 语言切换（英文）">
-        <pi-checkbox slot="right" active-mode="fill" shape="round" :value="true" />
-      </pi-list-item>
+      <pi-list title="语言切换">
+        <pi-list-item title="中文">
+          <pi-checkbox slot="right" active-mode="fill" shape="round" :value="true" />
+        </pi-list-item>
+        <pi-list-item title="英文">
+          <pi-checkbox slot="right" active-mode="fill" shape="round" />
+        </pi-list-item>
+      </pi-list>
     </scroll-view>
   </view>
 </template>
