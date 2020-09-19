1ff62d75f126b27929d6e3ea42a354bf5bf003da
zhangzhenfei <fei@sadais.com>
2020-09-19 17:24:03 +0800

    feat: piui更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script>
-import piPageTransitions from '@/piui/mixin/pi-page-transitions'
+import { create } from '@/piui/mixin/page-transitions'
 export default {
-  mixins: [piPageTransitions],
+  // H5环境全局mixin引入转场动画
+  // #ifdef H5
+  mixins: [
+    create({
+      'pages/base-style/index': 1,
+      'pages/components/index': 2,
+      'pages/tools/index': 3
+    })
+  ],
+  // #endif
   onLaunch: function() {},
   onShow: function() {},
   onHide: function() {}
