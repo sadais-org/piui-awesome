6e269d46728bc2d25a4c654c3f58909c06256371
zhangzhenfei <fei@sadais.com>
2020-09-23 10:37:28 +0800

    feat: 示例更新

--- src/pages/components/pages/line-progress/index.vue
+++ src/pages/components/pages/line-progress/index.vue
@@ -3,9 +3,13 @@
     <pi-navbar title="线型进度条" />
     <scroll-view class="pi-scroll" scroll-y="">
       <view class="pi-content">
+        <pi-section title="默认进度条" desc="30%" />
         <pi-line-progress percent="30" />
+        <pi-section title="默认进度条" desc="60%" />
         <pi-line-progress percent="60" />
+        <pi-section title="线条 striped" desc="90%" />
         <pi-line-progress percent="90" striped />
+        <pi-section title="线条活动 striped striped-active" desc="100%" />
         <pi-line-progress percent="100" striped striped-active />
       </view>
     </scroll-view>
