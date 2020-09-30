08d9c5a4730eec59429d33c4ab29db0fc591e881
zhangzhenfei <fei@sadais.com>
2020-09-30 14:41:49 +0800

    feat: build:h5加上分析工具

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 // vue.config.js
 module.exports = {
+  productionSourceMap: false,
   configureWebpack: {
     devtool: 'source-map'
+  },
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: process.env.NODE_ENV === 'production'
+    }
   }
-  // transpileDependencies: ['piui']
 }
