f383180565a6ebaf986ff6401c1c0b38bf074a7e
zhangzhenfei <fei@sadais.com>
2020-10-16 11:41:08 +0800

    feat: 部署演示库

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,17 @@
+// @ts-ignore
+/* eslint-disable */
 const gulp = require('gulp')
 const sftp = require('gulp-sftp-up4')
 
+// 上传商城静态资源
 gulp.task('upload:release', () =>
   gulp.src('dist/build/h5/**').pipe(
     sftp({
-      host: '120.79.208.202',
+      host: '120.79.19.132',
       port: '22',
       user: 'sadaisgulp',
       pass: '1b88ab6d!@#',
-      remotePath: `/mnt/suntrip_h5`
-    })
-  )
-)
-
-gulp.task('upload:images', () =>
-  gulp.src('src/static/image/**').pipe(
-    sftp({
-      host: '120.79.208.202',
-      port: '22',
-      user: 'sadaisgulp',
-      pass: '1b88ab6d!@#',
-      remotePath: `/mnt/suntrip_h5/static/image`
+      remotePath: '/mnt/piui/piui-awesome'
     })
   )
 )
