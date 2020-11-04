c612df3e86b0418b1db74553ea769406068c6b87
zhangzhenfei <fei@sadais.com>
2020-11-04 15:34:59 +0800

    feat: 完善pi-section演示

--- src/pages/components/pages/section/index.vue
+++ src/pages/components/pages/section/index.vue
@@ -3,9 +3,22 @@
     <pi-navbar title="section" />
     <scroll-view class="pi-scroll" scroll-y="">
       <view class="pi-content">
-        <pi-section title="周边设施" />
-        <pi-section title="学区学位" desc="助你一臂之力" />
-        <pi-section title="一条龙服务" extra-text="更多" show-right-icon />
+        <pi-section title="默认分块" />
+        <pi-section title="默认分块" desc="带描述" />
+        <pi-section title="显示右箭头" extra-text="更多" show-right-icon />
+        <pi-section title="不显示左侧竖线" extra-text="更多" :line="false" show-right-icon />
+        <pi-section title="显示下边框" extra-text="更多" border show-right-icon />
+        <pi-section>
+          Slot标题
+        </pi-section>
+        <pi-section>
+          Slot标题
+          <text slot="desc">slot desc</text>
+        </pi-section>
+        <pi-section show-right-icon :right-icon="{ name: 'next', color: '#cccccc' }">
+          <pi-loading show />
+          <text class="pi-mg-left-10">放个loading，改变右侧icon</text>
+        </pi-section>
       </view>
     </scroll-view>
   </view>
