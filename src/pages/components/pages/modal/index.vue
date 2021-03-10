4a102c1ba1c35698cd9271499dd69ab662aac0b4
liuweitao <tao@sadais.com>
2021-03-10 18:54:59 +0800

    fix: 完善pi-modal的demo

--- src/pages/components/pages/modal/index.vue
+++ src/pages/components/pages/modal/index.vue
@@ -2,13 +2,30 @@
   <view class="pi-scroll-container">
     <pi-navbar title="模态窗" />
     <scroll-view class="pi-scroll" scroll-y>
-      <view class="pi-content">
-        <pi-button type="primary" custom-class="pi-mg-top-12" @tap="handleShowModal">
+      <view class="pi-content pi-flex-wrap">
+        <pi-button type="primary" @tap="handleShowModal">
           显示
+        </pi-button>
+        <pi-button type="primary" custom-class="pi-mg-left-12" @tap="showModal2 = true">
+          定制确定按钮背景色
         </pi-button>
       </view>
     </scroll-view>
-    <pi-modal v-model="showModal" title="温馨提示" />
+    <pi-modal
+      v-model="showModal"
+      title="温馨提示"
+      content="是否要升级版本"
+      confirm-text="升级"
+      @confirm="handleUpdate"
+    />
+    <pi-modal
+      v-model="showModal2"
+      title="温馨提示"
+      content="是否要删除"
+      confirm-text="删除"
+      confirm-bg-color="#ff0000"
+      @confirm="handleDelete"
+    />
   </view>
 </template>
 
@@ -17,12 +34,19 @@ export default {
   name: 'Modal',
   data() {
     return {
-      showModal: false
+      showModal: false,
+      showModal2: false
     }
   },
   methods: {
     handleShowModal() {
       this.showModal = true
+    },
+    handleUpdate() {
+      console.log('点击升级了')
+    },
+    handleDelete() {
+      console.log('delete')
     }
   }
 }
