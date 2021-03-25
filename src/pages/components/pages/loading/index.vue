ba50b79c38a4cea8b3845d11ea541c09a7803799
qiushihao <qiushihao@sadais.com>
2021-03-25 17:13:39 +0800

    fix: 完善loading组件demo用例

--- src/pages/components/pages/loading/index.vue
+++ src/pages/components/pages/loading/index.vue
@@ -3,15 +3,33 @@
     <pi-navbar title="loading" />
     <scroll-view class="pi-scroll" scroll-y="">
       <view class="pi-content">
-        <pi-loading show />
+        <pi-section title="大小设置" />
         <pi-loading show size="50" />
+        <pi-loading show size="38" />
         <pi-loading show>加载中</pi-loading>
+        <pi-section title="颜色设置" />
         <pi-loading show size="50" color="#ff508a" />
+        <pi-loading show size="38" color="#c1c1c1" />
+        <pi-loading show>加载中</pi-loading>
+        <pi-section title="样式设置" />
         <pi-loading show color="#c1c1c1" type="round" />
         <pi-loading show color="#c1c1c1" />
         <pi-loading show color="#ff508a" type="round" />
         <pi-loading show color="#ff508a" type="round">加载中</pi-loading>
-        <pi-loading show size="38" type="round" vertical>加载中</pi-loading>
+        <pi-section title="垂直布局" />
+        <pi-loading show type="round" vertical>加载中</pi-loading>
+        <pi-section title="文字大小设置" />
+        <pi-loading show size="38" text-size="38" type="round" vertical>加载中</pi-loading>
+        <pi-section title="文字样式设置" />
+        <pi-loading
+          show
+          size="38"
+          :text-style="{ color: 'red', fontSize: '48rpx', fontWeight: 600 }"
+          type="round"
+          vertical
+        >
+          加载中
+        </pi-loading>
       </view>
     </scroll-view>
   </view>
