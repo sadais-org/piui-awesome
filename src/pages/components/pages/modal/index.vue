fcaa651bcd9fbcbbb72814f3942127076cabf3b3
刘远霖 <fei@sadais.com>
2021-06-02 21:57:36 +0800

    :sparkles: feat: 优化弹窗显示示例

--- src/pages/components/pages/modal/index.vue
+++ src/pages/components/pages/modal/index.vue
@@ -25,6 +25,10 @@
       v-model="showModal2"
       title="温馨提示"
       content="是否要删除"
+      :confirm-btn="{
+        text: '删除',
+        bgColor: '#ff0000'
+      }"
       confirm-text="删除"
       confirm-bg-color="#ff0000"
       @confirm="handleDelete"
@@ -33,26 +37,36 @@
       v-model="showModal3"
       title="温馨提示"
       content="暂无数据"
-      cancel-text="返回"
-      cancel-color="#FFFFFF"
-      cancel-bg-color="#5A7DFF"
-      :show-confirm-button="false"
+      :cancel-btn="{
+        text: '返回',
+        color: '#FFFFFF',
+        bgColor: '#5A7DFF'
+      }"
+      :confirm-btn="{
+        show: false
+      }"
       @cancel="handleCancel"
     />
     <pi-modal
       v-model="showModal4"
       title="温馨提示"
       content="隐藏关闭按钮"
-      :show-close-icon="false"
+      :popup="{
+        showCloseIcon: false
+      }"
     />
     <pi-modal
       v-model="showModal5"
       title="温馨提示"
       content="定制关闭按钮"
-      close-icon-name="back"
-      close-icon-color="#333333"
-      close-icon-position="tl"
-      close-icon-size="32"
+      :popup="{
+        closeIcon: {
+          name: 'back',
+          color: '#333333',
+          position: 'tl',
+          size: '32'
+        }
+      }"
     />
   </view>
 </template>
