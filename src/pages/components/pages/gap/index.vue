06ab102817572d96a1b8c8b2b9dfa194ce328b03
zhangzhenfei <fei@sadais.com>
2021-06-30 16:04:15 +0800

    :sparkles: feat: 完善gap示例库

--- src/pages/components/pages/gap/index.vue
+++ src/pages/components/pages/gap/index.vue
@@ -1,14 +1,23 @@
 <template>
   <view class="pi-scroll-container">
     <pi-navbar title="间隔槽" />
-    <scroll-view class="pi-scroll" scroll-y="">
-      <pi-section title="高度" padding="24rpx 32rpx" />
-      <pi-gap height="20" margin-bottom="10" />
-      <pi-gap height="5% " />
-      <pi-section title="背景颜色" padding="24rpx 32rpx" />
-      <pi-gap background="#f50" margin-bottom="10" />
-      <pi-gap background="#ff508a" margin-bottom="10" />
-      <pi-gap background="#5a7dff" />
+    <scroll-view class="pi-scroll" scroll-y>
+      <!-- 基础用法 -->
+      <pi-card>
+        <pi-section slot="title" padding="0" title="基础用法" />
+        <template slot="body">
+          <pi-gap />
+        </template>
+      </pi-card>
+      <!-- 基础用法 -->
+      <pi-card>
+        <pi-section slot="title" padding="0" title="背景颜色" />
+        <template slot="body">
+          <pi-gap background="#f50" margin-bottom="50" />
+          <pi-gap background="#ff508a" margin-bottom="50" />
+          <pi-gap background="#5a7dff" />
+        </template>
+      </pi-card>
     </scroll-view>
   </view>
 </template>
