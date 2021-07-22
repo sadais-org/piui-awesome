9507b35fcf17d6ae7b8594207b32ca8d039f620a
yangbo <yangbo@sadais.com>
2021-07-22 18:39:50 +0800

    feat: 图片和加载中

--- src/pages/components/pages/img/index.vue
+++ src/pages/components/pages/img/index.vue
@@ -2,46 +2,149 @@
   <view class="pi-scroll-container">
     <pi-navbar title="图片" />
     <scroll-view class="pi-scroll" scroll-y>
-      <view class="pi-content">
-        <pi-section>默认图片</pi-section>
-        <pi-img width="220" height="120" :custom-style="{ background: '#cccccc' }" :src="imgsrc" />
-        <pi-section>自动撑高</pi-section>
-        <pi-img
-          dot
-          width="220"
-          mode="widthFix"
-          :custom-style="{ background: '#cccccc' }"
-          :src="imgsrc"
-        />
-        <pi-section>红点</pi-section>
-        <pi-img
-          dot
-          width="220"
-          height="120"
-          :custom-style="{ background: '#cccccc' }"
-          :src="imgsrc"
-        />
-        <pi-section>5</pi-section>
-        <pi-img
-          width="220"
-          height="120"
-          :custom-style="{ background: '#cccccc' }"
-          badge="5"
-          :src="imgsrc"
-        />
-        <pi-section>99+</pi-section>
-        <pi-img
-          width="220"
-          height="120"
-          :custom-style="{ background: '#cccccc' }"
-          badge="99+"
-          :src="imgsrc"
-        />
-        <pi-section>加载失败</pi-section>
-        <pi-img width="220" height="120" :custom-style="{ background: '#cccccc' }" src="error" />
-        <pi-section>不传src</pi-section>
-        <pi-img width="220" height="120" :custom-style="{ background: '#cccccc' }" />
-      </view>
+      <pi-card>
+        <pi-section slot="title">基础用法</pi-section>
+        <template slot="body">
+          <view class="pi-justify-around">
+            <view>
+              <view class="pi-justify-center pi-mg-bottom-10">正常加载</view>
+              <pi-img
+                width="220"
+                height="120"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc"
+              />
+            </view>
+            <view>
+              <view class="pi-justify-center pi-mg-bottom-10">加载失败</view>
+              <pi-img
+                width="220"
+                height="120"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc + 1"
+              />
+            </view>
+            <view>
+              <view class="pi-justify-center pi-mg-bottom-10">自定占位内容</view>
+              <pi-img
+                width="220"
+                height="120"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc + 1"
+              >
+                <view slot="error">
+                  自定义占位内容
+                </view>
+              </pi-img>
+            </view>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title">懒加载</pi-section>
+        <template slot="body">
+          <view class="pi-align-center pi-justify-around">
+            <view>
+              <view class="pi-text-center">spinner-菊花'</view>
+              <pi-img
+                width="220"
+                height="200"
+                mode="widthFix"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc"
+              />
+            </view>
+            <view>
+              <view class="pi-text-center">round-圆环</view>
+              <pi-img
+                width="220"
+                height="200"
+                mode="widthFix"
+                loading-type="round"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc"
+              />
+            </view>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title">裁剪/缩放</pi-section>
+        <template slot="body">
+          <view class="pi-align-center pi-flex-column">
+            <view class="pi-mg-bottom-10">widthFix-高度自适应</view>
+            <pi-img
+              width="220"
+              mode="widthFix"
+              :custom-style="{ background: '#cccccc' }"
+              :src="imgsrc"
+            />
+          </view>
+          <view class="pi-flex-column-center pi-mg-top-20">
+            <view class="pi-mg-bottom-10">scaleToFill-不保持比例缩放</view>
+            <pi-img
+              width="220"
+              height="320"
+              mode="scaleToFill"
+              :custom-style="{ background: '#cccccc' }"
+              :src="imgsrc"
+            />
+          </view>
+          <view class="pi-flex-column-center pi-mg-top-20">
+            <view class="pi-mg-bottom-10">aspectFit-保持比例显示长边</view>
+            <pi-img
+              width="220"
+              height="220"
+              mode="aspectFit"
+              :custom-style="{ background: '#cccccc' }"
+              :src="imgsrc"
+            />
+          </view>
+          <view class="pi-flex-column-center pi-mg-top-20">
+            <view class="pi-mg-bottom-10">aspectFill-保持比例显示短边</view>
+            <pi-img
+              width="220"
+              height="220"
+              mode="aspectFill"
+              :custom-style="{ background: '#cccccc' }"
+              :src="imgsrc"
+            />
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title">图片形状</pi-section>
+        <template slot="body">
+          <view class="pi-align-center pi-justify-around">
+            <view>
+              <view class="pi-text-center">square-方形</view>
+              <pi-img
+                width="220"
+                height="220"
+                mode="widthFix"
+                shape="square"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc"
+              />
+            </view>
+            <view>
+              <view class="pi-text-center">round-圆形</view>
+              <pi-img
+                width="220"
+                height="220"
+                mode="scaleToFill"
+                loading-type="round"
+                shape="round"
+                :custom-style="{ background: '#cccccc' }"
+                :src="imgsrc"
+              />
+            </view>
+          </view>
+        </template>
+      </pi-card>
     </scroll-view>
   </view>
 </template>
--- src/pages/components/pages/loading/index.vue
+++ src/pages/components/pages/loading/index.vue
@@ -1,36 +1,56 @@
 <template>
   <view class="pi-scroll-container">
     <pi-navbar title="loading" />
-    <scroll-view class="pi-scroll" scroll-y="">
-      <view class="pi-content">
-        <pi-section title="大小设置" />
-        <pi-loading show size="50" />
-        <pi-loading show size="38" />
-        <pi-loading show>加载中</pi-loading>
-        <pi-section title="颜色设置" />
-        <pi-loading show size="50" color="#ff508a" />
-        <pi-loading show size="38" color="#c1c1c1" />
-        <pi-loading show>加载中</pi-loading>
-        <pi-section title="样式设置" />
-        <pi-loading show color="#c1c1c1" type="round" />
-        <pi-loading show color="#c1c1c1" />
-        <pi-loading show color="#ff508a" type="round" />
-        <pi-loading show color="#ff508a" type="round">加载中</pi-loading>
-        <pi-section title="垂直布局" />
-        <pi-loading show type="round" vertical>加载中</pi-loading>
-        <pi-section title="文字大小设置" />
-        <pi-loading show size="38" text-size="38" type="round" vertical>加载中</pi-loading>
-        <pi-section title="文字样式设置" />
-        <pi-loading
-          show
-          size="38"
-          :text-style="{ color: 'red', fontSize: '48rpx', fontWeight: 600 }"
-          type="round"
-          vertical
-        >
-          加载中
-        </pi-loading>
-      </view>
+    <scroll-view class="pi-scroll" scroll-y>
+      <pi-card>
+        <pi-section slot="title" title="基础用法" />
+        <template slot="body">
+          <view class="pi-justify-around">
+            <pi-loading show>示例1</pi-loading>
+            <pi-loading show vertical>示例2</pi-loading>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title" title="图标类型" />
+        <template slot="body">
+          <view class="pi-justify-around">
+            <pi-loading show type="spinner">示例3</pi-loading>
+            <pi-loading show type="round">示例4</pi-loading>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title" title="图标样式" />
+        <template slot="body">
+          <view class="pi-justify-around pi-align-center">
+            <pi-loading show type="round">示例5</pi-loading>
+            <pi-loading show type="round" color="#ff508a">示例6</pi-loading>
+            <pi-loading show type="round" color="#ff508a" size="50">示例7</pi-loading>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card>
+        <pi-section slot="title" title="图标样式" />
+        <template slot="body">
+          <view class="pi-justify-around pi-align-center">
+            <pi-loading show type="round" text-size="24">示例8</pi-loading>
+            <pi-loading show type="round" color="#ff508a" text-size="36">示例9</pi-loading>
+            <pi-loading
+              show
+              type="round"
+              color="#ff508a"
+              text-size="24"
+              :text-style="{ color: 'red', fontWeight: '700', fontSize: '48rpx' }"
+            >
+              示例10
+            </pi-loading>
+          </view>
+        </template>
+      </pi-card>
     </scroll-view>
   </view>
 </template>
