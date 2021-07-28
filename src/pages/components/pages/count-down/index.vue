a5b5e35ac06d22d2b07dc3798a2c2b2a93aa5123
yangbo <yangbo@sadais.com>
2021-07-28 09:29:13 +0800

    feat: 倒计时组件示例

--- src/pages/components/pages/count-down/index.vue
+++ src/pages/components/pages/count-down/index.vue
@@ -2,45 +2,106 @@
   <view class="pi-scroll-container">
     <pi-navbar title="倒计时" />
     <scroll-view class="pi-scroll" scroll-y="">
-      <view class="pi-content">
-        <pi-section>默认展示</pi-section>
-        <pi-count-down value="314159" />
-        <pi-section>不自动运行</pi-section>
-        <pi-count-down value="314159" :autoplay="false" />
-        <pi-section>不显示天</pi-section>
-        <pi-count-down value="314159" :show-day="false" />
-        <pi-section>不显示小时</pi-section>
-        <pi-count-down value="314159" :show-day="false" :show-hour="false" />
-        <pi-section>不显示分钟</pi-section>
-        <pi-count-down value="314159" :show-day="false" :show-hour="false" :show-minute="false" />
-        <pi-section>分隔符中文</pi-section>
-        <pi-count-down value="314159" separator="zh" />
-        <pi-section>自定义样式</pi-section>
-        <pi-count-down
-          value="314159"
-          separator="zh"
-          :time-style="{
-            color: '#CA332C',
-            background: '#FFE7E8',
-            border: '1px solid #CA332C'
-          }"
-          :separator-style="{
-            color: '#CA332C'
-          }"
-        />
-        <pi-section>自定义样式</pi-section>
-        <pi-count-down
-          value="314159"
-          :time-style="{
-            color: '#CA332C',
-            background: '#FFE7E8',
-            border: '1px solid #CA332C'
-          }"
-          :separator-style="{
-            color: '#CA332C'
-          }"
-        />
-      </view>
+      <pi-card padding="0">
+        <pi-section slot="title" padding="24">
+          基础用法
+        </pi-section>
+        <template slot="body">
+          <view class="pi-pd-24">
+            <pi-count-down value="314159" />
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card padding="0">
+        <pi-section slot="title" padding="24">
+          停止倒计时
+        </pi-section>
+        <template slot="body">
+          <view class="pi-pd-24">
+            <pi-count-down value="314159" :autoplay="false" />
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card padding="0">
+        <pi-section slot="title" padding="24">
+          不同精度
+        </pi-section>
+        <template slot="body">
+          <view class="pi-pd-24">
+            <view>
+              不显示天：
+              <pi-count-down value="314159" :show-day="false" />
+            </view>
+            <view class="pi-mg-tb-24">
+              不显示小时：
+              <pi-count-down value="314159" :show-day="false" :show-hour="false" />
+            </view>
+            <view>
+              不显示分钟：
+              <pi-count-down
+                value="314159"
+                :show-day="false"
+                :show-hour="false"
+                :show-minute="false"
+              />
+            </view>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card padding="0">
+        <pi-section slot="title" padding="24">
+          分隔符
+        </pi-section>
+        <template slot="body">
+          <view class="pi-pd-24">
+            <view>
+              分隔符(colon-冒号)：
+              <pi-count-down value="314159" separator="colon" />
+            </view>
+            <view class="pi-mg-tb-24">
+              分隔符(zh-中文)：
+              <pi-count-down value="314159" separator="zh" />
+            </view>
+          </view>
+        </template>
+      </pi-card>
+
+      <pi-card padding="0">
+        <pi-section slot="title" padding="24">
+          自定义样式
+        </pi-section>
+        <template slot="body">
+          <view class="pi-pd-24">
+            <view>
+              时间项：
+              <pi-count-down
+                value="314159"
+                :time-style="{
+                  color: '#CA332C',
+                  background: '#FFE7E8',
+                  border: '1px solid #CA332C'
+                }"
+                :separator-style="{
+                  color: '#CA332C'
+                }"
+              />
+            </view>
+            <view class="pi-mg-tb-24">
+              分隔符：
+              <pi-count-down
+                value="314159"
+                separator="zh"
+                :separator-style="{
+                  color: '#CA332C'
+                }"
+              />
+            </view>
+          </view>
+        </template>
+      </pi-card>
     </scroll-view>
   </view>
 </template>
