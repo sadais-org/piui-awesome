0f013eda80391f7ae8d5541db9d92b7d2ad29ce2
zhangzhenfei <fei@sadais.com>
2021-08-27 10:44:56 +0800

    :bookmark: release: 优化示例库

--- src/pages/components/index.vue
+++ src/pages/components/index.vue
@@ -2,25 +2,28 @@
   <view class="pi-scroll-container">
     <pi-navbar :border-bottom="false">组件</pi-navbar>
     <scroll-view class="pi-scroll" scroll-y>
-      <view class="pi-content">
-        <pi-grid square col="3" gap="24">
-          <!-- pi-grid-item square 是不用传的，但是头条小程序不支持provide inject，所以这里还是得传-->
-          <pi-grid-item
-            v-for="(components, index) in baseComponents"
-            :key="components.name"
-            :index="index"
-            bg-color="#ffffff"
-            square
-          >
-            <view
-              class="pi-h-100P pi-flex-column-center"
-              @tap.stop="handleNavigate(components.name)"
+      <pi-card v-for="comp in components" :key="comp.name">
+        <pi-section slot="title" padding="0" :title="comp.title" />
+        <template slot="body">
+          <pi-grid square col="3" gap="24">
+            <!-- pi-grid-item square 是不用传的，但是头条小程序不支持provide inject，所以这里还是得传-->
+            <pi-grid-item
+              v-for="(component, index) in comp.components"
+              :key="component.name"
+              :index="index"
+              bg-color="#ffffff"
+              square
             >
-              {{ components.zhName }}
-            </view>
-          </pi-grid-item>
-        </pi-grid>
-      </view>
+              <view
+                class="pi-h-100P pi-flex-column-center"
+                @tap.stop="handleNavigate(component.name)"
+              >
+                {{ component.zhName }}
+              </view>
+            </pi-grid-item>
+          </pi-grid>
+        </template>
+      </pi-card>
     </scroll-view>
   </view>
 </template>
@@ -31,163 +34,212 @@ export default {
     return {}
   },
   computed: {
-    baseComponents() {
+    components() {
       return [
         {
-          name: 'navbar',
-          zhName: '导航栏'
+          name: 'layout',
+          title: '布局组件',
+          components: [
+            {
+              name: 'status-bar',
+              zhName: '状态栏'
+            },
+            {
+              name: 'bottom-bar',
+              zhName: '底部按钮栏'
+            },
+            {
+              name: 'navbar',
+              zhName: '导航栏'
+            },
+            {
+              name: 'gap',
+              zhName: '间隔槽'
+            },
+            {
+              name: 'section',
+              zhName: '区块'
+            },
+            {
+              name: 'grid',
+              zhName: '宫格'
+            },
+            {
+              name: 'list',
+              zhName: '列表'
+            },
+            {
+              name: 'swiper',
+              zhName: '轮播'
+            },
+            {
+              name: 'list',
+              zhName: '列表'
+            },
+            {
+              name: 'card',
+              zhName: '卡片'
+            },
+            {
+              name: 'waterfall',
+              zhName: '瀑布流'
+            },
+            {
+              name: 'marquee',
+              zhName: '跑马灯'
+            }
+          ]
         },
         {
-          name: 'search',
-          zhName: '搜索框'
-        },
-        {
-          name: 'mask',
-          zhName: '遮罩层'
+          name: 'basic',
+          title: '基础组件',
+          components: [
+            {
+              name: 'icon',
+              zhName: '图标'
+            },
+            {
+              name: 'badge',
+              zhName: '徽标'
+            },
+            {
+              name: 'img',
+              zhName: '图片'
+            },
+            {
+              name: 'search',
+              zhName: '搜索框'
+            },
+            {
+              name: 'divider',
+              zhName: '分割线'
+            },
+            {
+              name: 'loading',
+              zhName: '加载中'
+            },
+            {
+              name: 'img',
+              zhName: '图片'
+            },
+            {
+              name: 'button',
+              zhName: '按钮'
+            },
+            {
+              name: 'line-progress',
+              zhName: '线型进度条'
+            },
+            {
+              name: 'count-down',
+              zhName: '倒计时'
+            },
+            {
+              name: 'swiper',
+              zhName: '轮播图'
+            },
+            {
+              name: 'skeleton',
+              zhName: '骨架屏'
+            }
+          ]
         },
         {
           name: 'popup',
-          zhName: '弹出层'
-        },
-        {
-          name: 'popup-select',
-          zhName: '弹出选择'
-        },
-        {
-          name: 'divider',
-          zhName: '分割线'
-        },
-        {
-          name: 'loading',
-          zhName: '加载中'
-        },
-        {
-          name: 'calendar-panel',
-          zhName: '日历面板'
-        },
-        {
-          name: 'calendar',
-          zhName: '日历'
-        },
-        {
-          name: 'button',
-          zhName: '按钮'
-        },
-        {
-          name: 'select',
-          zhName: '选择器'
-        },
-        {
-          name: 'img',
-          zhName: '图片'
-        },
-        {
-          name: 'modal',
-          zhName: '模态窗'
-        },
-        {
-          name: 'checkbox',
-          zhName: '复选框'
-        },
-        {
-          name: 'radio',
-          zhName: '单选框'
-        },
-        {
-          name: 'tabs',
-          zhName: '标签栏'
-        },
-        {
-          name: 'switch',
-          zhName: '开关'
-        },
-        {
-          name: 'section',
-          zhName: '区块'
-        },
-        {
-          name: 'line-progress',
-          zhName: '线型进度条'
-        },
-        {
-          name: 'gap',
-          zhName: '间隔槽'
-        },
-        {
-          name: 'count-down',
-          zhName: '倒计时'
-        },
-        {
-          name: 'date-picker',
-          zhName: '日期选择器'
-        },
-        {
-          name: 'swiper',
-          zhName: '轮播图'
-        },
-        {
-          name: 'regions-select',
-          zhName: '地区选择'
-        },
-        {
-          name: 'picker',
-          zhName: '选择器'
-        },
-        {
-          name: 'stepper',
-          zhName: '步进器'
-        },
-        {
-          name: 'code-input',
-          zhName: '校验码输入'
+          title: '弹窗组件',
+          components: [
+            {
+              name: 'mask',
+              zhName: '遮罩层'
+            },
+            {
+              name: 'popup',
+              zhName: '弹出层'
+            },
+            {
+              name: 'popup-select',
+              zhName: '弹出选择'
+            },
+            {
+              name: 'calendar',
+              zhName: '日历'
+            },
+            {
+              name: 'select',
+              zhName: '选择器'
+            },
+            {
+              name: 'modal',
+              zhName: '模态窗'
+            },
+            {
+              name: 'date-picker',
+              zhName: '日期选择器'
+            },
+            {
+              name: 'regions-select',
+              zhName: '地区选择'
+            },
+            {
+              name: 'picker',
+              zhName: '选择器'
+            }
+          ]
         },
         {
           name: 'form',
-          zhName: '表单'
-        },
-        {
-          name: 'input',
-          zhName: '输入框'
-        },
-        {
-          name: 'skeleton',
-          zhName: '骨架屏'
-        },
-        {
-          name: 'rate',
-          zhName: '评分'
-        },
-        {
-          name: 'waterfall',
-          zhName: '瀑布流'
-        },
-        {
-          name: 'number-keyboard',
-          zhName: '数字键盘'
-        },
-        {
-          name: 'status-bar',
-          zhName: '状态栏'
-        },
-        {
-          name: 'bottom-bar',
-          zhName: '底部按钮栏'
-        },
-        {
-          name: 'list',
-          zhName: '列表'
-        },
-        {
-          name: 'card',
-          zhName: '卡片'
-        },
-        {
-          name: 'upload-img',
-          zhName: '图片上传'
-        },
-        {
-          name: 'marquee',
-          zhName: '跑马灯'
+          title: '表单组件',
+          components: [
+            {
+              name: 'form',
+              zhName: '表单'
+            },
+            {
+              name: 'calendar-panel',
+              zhName: '日历面板'
+            },
+            {
+              name: 'checkbox',
+              zhName: '复选框'
+            },
+            {
+              name: 'radio',
+              zhName: '单选框'
+            },
+            {
+              name: 'tabs',
+              zhName: '标签栏'
+            },
+            {
+              name: 'switch',
+              zhName: '开关'
+            },
+            {
+              name: 'code-input',
+              zhName: '校验码输入'
+            },
+            {
+              name: 'stepper',
+              zhName: '步进器'
+            },
+            {
+              name: 'input',
+              zhName: '输入框'
+            },
+            {
+              name: 'rate',
+              zhName: '评分'
+            },
+
+            {
+              name: 'number-keyboard',
+              zhName: '数字键盘'
+            },
+            {
+              name: 'upload-img',
+              zhName: '图片上传'
+            }
+          ]
         }
       ]
     }
