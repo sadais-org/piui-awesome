c45f228dd006e87a364f819e233d376b3d2214bf
zhangzhenfei <fei@sadais.com>
2021-09-24 17:04:07 +0800

    :sparkles: feat: 示例更新

--- src/pages/components/pages/pagination/index.vue
+++ src/pages/components/pages/pagination/index.vue
@@ -4,45 +4,63 @@
     <scroll-view class="pi-scroll pi-flex-column" scroll-y>
       <!-- 基础用法 -->
       <pi-card>
-        <pi-section slot="title" padding="0" title="基础用法" desc=" " />
+        <pi-section slot="title" padding="0" title="基础用法" />
         <template slot="body">
           <pi-pagination :total="300" :limit="7" :page.sync="page" :page-size="20" />
         </template>
       </pi-card>
       <pi-card>
-        <pi-section slot="title" padding="0" title="基础用法" desc="背景色" />
+        <pi-section slot="title" padding="0" title="填充模式" desc="fill" />
         <template slot="body">
-          <pi-pagination :total="300" :limit="5" :page.sync="page1" :page-size="20" background />
+          <pi-pagination
+            :total="300"
+            :limit="5"
+            :page.sync="page1"
+            :page-size="20"
+            active-mode="fill"
+          />
         </template>
       </pi-card>
       <pi-card>
-        <pi-section
-          slot="title"
-          padding="0"
-          title="基础用法"
-          desc="背景色+右对齐(text-align=right)"
-        />
+        <pi-section slot="title" padding="0" title="填充模式+右对齐" desc="align=right" />
         <template slot="body">
           <pi-pagination
             :total="300"
             :limit="5"
             :page.sync="page2"
             :page-size="20"
-            background
-            text-align="right"
+            active-mode="fill"
+            align="right"
           />
         </template>
       </pi-card>
       <pi-card>
-        <pi-section slot="title" padding="0" title="基础用法" desc="通过插槽改变上一页、下一页" />
+        <pi-section slot="title" padding="0" title="item插槽" desc="slot=item" />
+        <template slot="body">
+          <pi-pagination :total="300" :page.sync="page3" :page-size="20">
+            <template slot="item" slot-scope="{ item, active }">
+              <view class="pi-fz-32" :class="active ? 'pi-fz-40 pi-fz-500 pi-solid-bottom-6' : ''">
+                {{ item }}
+              </view>
+            </template>
+          </pi-pagination>
+        </template>
+      </pi-card>
+      <pi-card>
+        <pi-section
+          slot="title"
+          padding="0"
+          title="上一页、下一页插槽"
+          desc="slot=prev，slot=next"
+        />
         <template slot="body">
           <pi-pagination
             :total="300"
             :limit="5"
-            :page.sync="page3"
+            :page.sync="page4"
             :page-size="20"
-            background
-            text-align="left"
+            active-mode="fill"
+            align="left"
           >
             <pi-icon slot="prev" name="back" size="32" />
             <pi-icon slot="next" name="right" size="32" />
@@ -61,8 +79,15 @@ export default {
       page: 2,
       page1: 1,
       page2: 1,
-      page3: 3
+      page3: 3,
+      page4: 8
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+::v-deep .pi-solid-bottom-6::after {
+  border-bottom-color: $pi-primary-color;
+}
+</style>
